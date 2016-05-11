fdabc4c188b796686be913764859041d2736ddbc
Blealtan <bolee159632@live.com>
2016-05-11 18:51:18 +0800

    HardFault fixed.

--- src/main/include/tasks.h
+++ src/main/include/tasks.h
@@ -6,13 +6,13 @@
 
 #define INIT_TASK_PRIO 1
 void init_task(void *);
-extern OS_STK init_stack[MaxStkSize];
+static OS_STK init_stack[MaxStkSize];
 
 #define BLE_SERVICE_TASK_PRIO 2
 void ble_service_task(void *);
-extern OS_STK ble_service_stack[MaxStkSize];
+static OS_STK ble_service_stack[MaxStkSize];
 #define BLE_SERVICE_QUEUE_LENGTH 128
 
 #define LED_BLINK_TASK_PRIO 3
 void led_blink_task(void *);
-extern OS_STK led_blink_stack[MaxStkSize];
+static OS_STK led_blink_stack[MaxStkSize];
--- src/main/src/led_blink.c
+++ src/main/src/led_blink.c
@@ -4,19 +4,14 @@
 
 #include "tasks.h"
 
-OS_STK led_blink_stack[MaxStkSize];
-
 void led_blink_task(void * p)
 {
     OS_EVENT *ble_service_queue = (OS_EVENT *)p;
 	
-	OSTimeDlyHMSM(0, 0, 0, 500);
+    GPIO_SetActive(GPIO_LED_PORT, GPIO_LED_PIN);
 
     while (1)
     {
-        GPIO_SetActive(GPIO_LED_PORT, GPIO_LED_PIN);
-        OSTimeDlyHMSM(0, 0, 1, 0);
-        GPIO_SetInactive(GPIO_LED_PORT, GPIO_LED_PIN);
         OSTimeDlyHMSM(0, 0, 1, 0);
 		OSQPost(ble_service_queue, NULL);
     }
