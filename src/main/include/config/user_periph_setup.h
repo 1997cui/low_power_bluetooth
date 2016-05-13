12601ee5921d7fc4bfa57aa58c9710160a822360
1997cui <1997cui@sina.com>
2016-05-13 22:37:26 +0800

    暴力提交，人人有责。编译过了，链接挂了。有两个config待修。
    如上文所示，user_periphel_config.c 有两个，待merge

--- src/main/include/config/user_periph_setup.h
+++ src/main/include/config/user_periph_setup.h
@@ -42,6 +42,37 @@
 /* Enable WKUPCT. Required by wkupct_quadec driver. */
     #define WKUP_ENABLED
 
+
+/*
+ * DEFINES
+ ****************************************************************************************
+ */
+
+//*** <<< Use Configuration Wizard in Context Menu >>> ***
+
+// <o> DK selection <0=> As in da1458x_periph_setup.h <1=> Basic <2=> Pro <3=> Expert
+    #define HW_CONFIG (0)
+
+    #define HW_CONFIG_BASIC_DK  ((HW_CONFIG==0 && SDK_CONFIG==1) || HW_CONFIG==1)
+    #define HW_CONFIG_PRO_DK    ((HW_CONFIG==0 && SDK_CONFIG==2) || HW_CONFIG==2)
+    #define HW_CONFIG_EXPERT_DK ((HW_CONFIG==0 && SDK_CONFIG==3) || HW_CONFIG==3)
+
+//*** <<< end of configuration section >>>    ***
+
+
+/****************************************************************************************/
+/* i2c eeprom configuration                                                             */
+/****************************************************************************************/
+
+    #define I2C_EEPROM_SIZE   0x20000         // EEPROM size in bytes
+    #define I2C_EEPROM_PAGE   256             // EEPROM's page size in bytes
+    #define I2C_SPEED_MODE    I2C_FAST        // 1: standard mode (100 kbits/s), 2: fast mode (400 kbits/s)
+    #define I2C_ADDRESS_MODE  I2C_7BIT_ADDR   // 0: 7-bit addressing, 1: 10-bit addressing
+    #define I2C_ADDRESS_SIZE  I2C_2BYTES_ADDR // 0: 8-bit memory address, 1: 16-bit memory address, 3: 24-bit memory address
+
+
+
+
 /****************************************************************************************/ 
 /* UART configuration                                                                   */
 /****************************************************************************************/  
@@ -66,6 +97,24 @@
         #define  GPIO_UART2_RX_PORT     GPIO_PORT_1
         #define  GPIO_UART2_RX_PIN      GPIO_PIN_3
     #endif
+		
+		
+/****************************************************************************************/
+/* LED configuration                                                                    */
+/****************************************************************************************/
+
+    #if HW_CONFIG_BASIC_DK
+        #define GPIO_LED_PORT     GPIO_PORT_1
+        #define GPIO_LED_PIN      GPIO_PIN_0
+    #elif HW_CONFIG_PRO_DK
+        #define GPIO_LED_PORT     GPIO_PORT_1
+        #define GPIO_LED_PIN      GPIO_PIN_0
+    #else // HW_CONFIG_EXPERT_DK
+        #define GPIO_LED_PORT     GPIO_PORT_0
+        #define GPIO_LED_PIN      GPIO_PIN_7
+    #endif
+	
+	
 /*
  * FUNCTION DECLARATIONS
  ****************************************************************************************
